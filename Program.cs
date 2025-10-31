5e9a6ca7c6c6c4442e4e5dfbf4430c2c6478676c
artemmail <artemmail@gmail.com>
2025-10-31 23:39:17 +0300

    Modernize .NET and C++ project configurations

--- Program.cs
+++ Program.cs
@@ -1,20 +1,18 @@
 using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace Goo
 {
-	static class Program
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
-		}
-	}
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
 }
